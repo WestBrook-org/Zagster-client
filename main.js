7e825f0ee86fdf74c4be19a0d6b0c9498f1ae9e2
Brook West <bmwest42@gmail..com>
2019-11-20 15:20:27 -0800

    Milestone 1

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
-alert(woooop!)
+const BASE_URL = "https://zagster-service.herokuapp.com"
+
+$(updateView)
+
 function updateView() {
-    alert("Using jQuery")
+  $.getJSON(BASE_URL + "/rides/count" , updateRideCount)
+}
+
+function updateRideCount(data) {
+  numberOfRides = data.count
+  $("h2#rideCount").html(numberOfRides)
 }
