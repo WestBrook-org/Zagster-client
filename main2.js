0e4f1485bd8dc8eccf61d051f3c559a81e70bd47
Brook West <bmwest42@gmail..com>
2019-12-03 16:54:13 -0800

    aesthetic complete

--- main2.js
+++ main2.js
@@ -1,23 +1,15 @@
-   // a constant's value can never change.
+
    const BASE_URL = "https://zagster-service.herokuapp.com"
 
-   //call function updateView() after the page loads
    $(updateView)
-   
-   
-   //Arrays to hold data points pulled from JQUERY
+
    var my_data = []
-   
-   //updateView() function definition
-   //this function gets JSON data from the web server endpoint /rides/count/per_month and calls the function perYear()
-   //after the function perYear runs it will call the function updateChart()
+  
    function updateView() {
      
      $.when ($.getJSON(BASE_URL + "/rides/count/per_month", perYear), 
        ).then(updateChart);
    }
-   
-   
    // This is what your JSON data looks like given the endpoint /rides/count/per_month 
    //If you type https://zagster-service.herokuapp.com/rides/count/per_month in your web browser, you'll see this data
    // {"2016":[{"9":220},{"10":141},{"11":89},{"12":16}],
@@ -26,10 +18,8 @@
    
    function perYear(data) {
    
-   
        var data2018 = data[2018] 
    
-      
        console.log(data2018[0][1])
        my_data.push(data2018[0][1])
        console.log(data2018[1][2]) 
@@ -49,30 +39,25 @@
        console.log(data2018[8][9]) 
        my_data.push(data2018[8][9])
        console.log(data2018[9][10]) 
-       my_data.push(data2018[9][10])
-     
-        
+       my_data.push(data2018[9][10])    
      }
    
    function updateChart() {
        var ctx = document.getElementById('myChart').getContext('2d');
        var chart = new Chart(ctx, {
-           // The type of chart we want to create
-           type: 'doughnut',
-   
-           // The data for our dataset
+         
+           type: 'pie',
            data: {
                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August','September', 'October'],
                datasets: [{
-                   label: 'Zagster Number of Rides Per Month in 2018',
-                   backgroundColor: 'rgb(128, 0, 128)',
+                   backgroundColor: ['rgb(225,0,0)', 'rgb(225,20,147)', 'rgb(225,140,0)', 'rgb(225,225,0)', 'rgb(0,225,0)', 'rgb(0,225,127)','rgb(0,225,225)', 'rgb(0,0,225)', 'rgb(225,0,225)', 'rgb(148,0,211)'],
                    borderColor: 'rgb(245, 245, 245)',
-                   //data: [0, 10, 5, 2, 20, 30, 45]
-                   data: my_data  //make the chart use your my_data array
+                   data: my_data
+                   
                }]
            },
-   
-           // Configuration options go here
            options: {}
        });
    }
+
+ 
