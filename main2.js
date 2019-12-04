5657ea50106538b01988171762f48e4ab85057e5
Brook West <bmwest42@gmail..com>
2019-12-03 18:30:24 -0800

    About page done

--- main2.js
+++ main2.js
@@ -10,12 +10,7 @@
      $.when ($.getJSON(BASE_URL + "/rides/count/per_month", perYear), 
        ).then(updateChart);
    }
-   // This is what your JSON data looks like given the endpoint /rides/count/per_month 
-   //If you type https://zagster-service.herokuapp.com/rides/count/per_month in your web browser, you'll see this data
-   // {"2016":[{"9":220},{"10":141},{"11":89},{"12":16}],
-   // "2017":[{"1":20},{"2":31},{"3":79},{"4":88},{"5":156},{"6":301},{"7":405},{"8":187},{"9":401},{"10":353},{"11":113},{"12":79}],
-   // "2018":[{"1":94},{"2":83},{"3":154},{"4":1216},{"5":2400},{"6":1756},{"7":2748},{"8":1912},{"9":3648},{"10":2404}]}
-   
+  
    function perYear(data) {
    
        var data2018 = data[2018] 
